e940f79a34f7989c689daa3c3955533613e4ce2c
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2019-02-14 20:04:59 -0200

    add program skeleton

--- transitivereflexive/lib/trm.ex
+++ transitivereflexive/lib/trm.ex
@@ -15,4 +15,35 @@ defmodule TRM do
   def hello do
     :world
   end
+
+  @doc """
+
+  """
+  def closure_algorithm do
+    graph = [{1,2},{2,3}]
+    matrix = create_adjacent_matrix(graph)
+    matrix = add_main_diagonal(matrix)
+    result = find_nodes(matrix)
+  end
+
+  @doc """
+
+  """
+  def create_adjacent_matrix(graph) do
+
+  end
+
+  @doc """
+
+  """
+  def add_main_diagonal(matrix) do
+
+  end
+
+  @doc """
+
+  """
+  def find_nodes(matrix) do
+
+  end
 end
