f8c9b1d8b18badc377bcc1788765243822bd51af
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2019-03-05 23:50:15 -0300

    Add chain generation and recognition functions

--- ep2/lib/chainrec.ex
+++ ep2/lib/chainrec.ex
@@ -30,7 +30,21 @@ defmodule CHAINREC do
   @doc """
   What to do once the list is empty
   """
-  def rec_parse(set_list) when set_list = nil do
+  def rec_parse(set_list) when set_list == nil do
     IO.puts("end of list")
+  end
+
+  @doc """
+  Generates chain with grammar and maximum size
+  """
+  def gen_chain(grammar, max_size) do
+    true
+  end
+
+  @doc """
+  Check if a chain is valid given a certain grammar
+  """
+  def recognition(chain, grammar, max_size) do
+    true
   end
 end
