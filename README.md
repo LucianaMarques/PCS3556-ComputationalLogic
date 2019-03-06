64b9ef8d8d379d438d3c7a87ba11bface9211d3f
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2019-03-05 22:49:41 -0300

    Add recursive set list parse function

--- README.md
+++
@@ -1,2 +0,0 @@
-# PCS3556-ComputationalLogic
-This repository will contain all my work for my Computational Logic class.
--- ep2/lib/chainrec.ex
+++ ep2/lib/chainrec.ex
@@ -15,4 +15,22 @@ defmodule CHAINREC do
   def hello do
     :world
   end
+
+  @doc """
+  Parse a set recursively and returns one of its elements
+  """
+  def rec_parse(set_list) do
+    IO.puts(set_list)
+    [head| tail] = set_list
+    head
+    List.delete_at(set_list,0)
+    rec_parse(set_list)
+  end
+
+  @doc """
+  What to do once the list is empty
+  """
+  def rec_parse(set_list) when set_list = nil do
+    IO.puts("end of list")
+  end
 end
