d67997cb00bb941fac576c3ebb90650a3adc068b
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2019-03-23 19:58:20 -0300

    First program version

--- ep3/determ/determ/lib/determautomata.ex
+++ ep3/determ/determ/lib/determautomata.ex
@@ -4,15 +4,53 @@ defmodule DETERMAUTOMATA do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DETERMAUTOMATA.hello()
-      :world
-
+  Automaton's main functionality
   """
-  def hello do
-    :world
+  def determ_automaton(tape) do
+    current_state = :q0
+    Enum.map(tape, fn x -> current_state = automata_transition(current_state,x) end)
+    IO.puts(current_state)
+  end
+
+  @doc """
+  Computes the automaton's transitions.
+  q2 is accept state, all other are rejection
+  """
+  def automata_transition(current_state, input) do
+    if (current_state == :q0) do
+      if (input == :a) do
+        :q1
+      else
+        :q2
+      end
+    end
+    if (current_state == :q1) do
+      if (input == :a) do
+        :q3
+      else
+        :q2
+      end
+    end
+    if (current_state == :q2) do
+      :q2
+    end
+    if (current_state == :q3) do
+      :q3
+    end
+  end
+
+  @doc """
+  Gets the character at the current position of tape
+  May be nil in case tape is over
+  """
+  def read(tape,i) do
+    String.at(tape,i)
+  end
+
+  @doc """
+  Moves current position of tape
+  """
+  def reconfig(i) do
+    i + 1
   end
 end
--- ep3/determ/determ/test/determautomata_test.exs
+++ ep3/determ/determ/test/determautomata_test.exs
@@ -2,7 +2,12 @@ defmodule DETERMAUTOMATATest do
   use ExUnit.Case
   doctest DETERMAUTOMATA
 
-  test "greets the world" do
-    assert DETERMAUTOMATA.hello() == :world
+  test "iterate through tape" do
+    # creates tape
+    tape = String.codepoints("ab")
+    # iterates through tape
+    Enum.map(tape, fn x -> IO.puts(x) end)
   end
+
+
 end
